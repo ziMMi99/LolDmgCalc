d8741fee6d6245dbe1172605a8ff56d681cefe75
ziMMi99 <simon@mvbmail.dk>
2024-09-19 13:57:32 +0200

    Initial commit

---
+++ Items/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Items.ItemExplorer.StarterItems;
+
+namespace Items {
+	public abstract class Item {
+		
+	}
+}
---
+++ Items/StatType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Items {
+	internal enum StatType {
+		Health,
+		Mana,
+		AttackDamage,
+		AbilityPower,
+		Armor,
+		MagicResistance,
+		AttackSpeed,
+		CriticalStrikeChance,
+		LifeSteal,
+		MovementSpeed,
+		CooldownReduction,
+		ManaRegeneration,
+		HealthRegeneration,
+		Tenacity,
+		SlowResistance,
+		ArmorPenetration,
+		MagicPenetration,
+		SpellVamp,
+		HealthPerLevel,
+		ManaPerLevel,
+		AttackDamagePerLevel,
+		AbilityPowerPerLevel,
+		ArmorPerLevel,
+		MagicResistancePerLevel,
+		AttackSpeedPerLevel,
+		CriticalStrikeChancePerLevel,
+		LifeStealPerLevel,
+		MovementSpeedPerLevel,
+		CooldownReductionPerLevel,
+		ManaRegenerationPerLevel,
+		HealthRegenerationPerLevel,
+		TenacityPerLevel,
+		SlowResistancePerLevel,
+		ArmorPenetrationPerLevel,
+		MagicPenetrationPerLevel,
+		SpellVampPerLevel	
+	}
+}

d8f255cb328ab747a97361b79bf8f9cdb5ca0bab
SebErSej <Crazi.Seb@gmail.com>
2024-09-30 12:38:44 +0200

    Continued work on Champion.cs and Cho_Gath.cs
    noteable things done:
    - Made more utilityMethods for working with levels and ability ranks
    - Added comments and summaries
    - Major simplification to the GetStat method in Champion

--- Champions/Champion.cs
+++ Champions/Champion.cs
@@ -1,12 +1,14 @@
 ﻿using Stats;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Champions {
     public abstract class Champion {
 
         //----------Variables-----------
         internal int level;
-        internal int qLevel, wLevel, eLevel, rLevel;
+        internal int qRank, wRank, eRank, rRank;
         internal StatHandler baseStats;
         internal StatHandler statPerLevel;
 
@@ -55,78 +57,117 @@ namespace Champions {
 
         //----------Getters&Setters---------------
         public virtual double GetStat(StatType statType) {
-            switch (statType) {
-                case StatType.Health:
-                return baseStats.Get(StatType.Health) + (statPerLevel.Get(StatType.Health) * level);
+			return baseStats.Get(statType) + (statPerLevel.Get(statType) * level);
+		}
 
-                case StatType.Mana:
-                return baseStats.Get(StatType.Mana) + (statPerLevel.Get(StatType.Mana) * level);
-
-                case StatType.AttackDamage:
-                return baseStats.Get(StatType.AttackDamage) + (statPerLevel.Get(StatType.AttackDamage) * level);
-
-                case StatType.AbilityPower:
-                return baseStats.Get(StatType.AbilityPower) + (statPerLevel.Get(StatType.AbilityPower) * level);
-
-                case StatType.Armor:
-                return baseStats.Get(StatType.Armor) + (statPerLevel.Get(StatType.Armor) * level);
-
-                case StatType.MagicResistance:
-                return baseStats.Get(StatType.MagicResistance) + (statPerLevel.Get(StatType.MagicResistance) * level);
-
-                case StatType.AttackSpeed: //Todo: lav beregningen rigtigt
-                return baseStats.Get(StatType.AttackSpeed) + (statPerLevel.Get(StatType.AttackSpeed) * level);
-
-                case StatType.CriticalStrikeChance:
-                return baseStats.Get(StatType.CriticalStrikeChance) + (statPerLevel.Get(StatType.CriticalStrikeChance) * level);
-
-                case StatType.LifeSteal:
-                return baseStats.Get(StatType.LifeSteal) + (statPerLevel.Get(StatType.LifeSteal) * level);
-
-                case StatType.MovementSpeed:
-                return baseStats.Get(StatType.MovementSpeed) + (statPerLevel.Get(StatType.MovementSpeed) * level);
-
-                case StatType.CooldownReduction:
-                return baseStats.Get(StatType.CooldownReduction) + (statPerLevel.Get(StatType.CooldownReduction) * level);
-
-                case StatType.ManaRegeneration:
-                return baseStats.Get(StatType.ManaRegeneration) + (statPerLevel.Get(StatType.ManaRegeneration) * level);
-
-                case StatType.HealthRegeneration:
-                return baseStats.Get(StatType.HealthRegeneration) + (statPerLevel.Get(StatType.HealthRegeneration) * level);
-
-                case StatType.Tenacity:
-                return baseStats.Get(StatType.Tenacity) + (statPerLevel.Get(StatType.Tenacity) * level);
-
-                case StatType.SlowResistance:
-                return baseStats.Get(StatType.SlowResistance) + (statPerLevel.Get(StatType.SlowResistance) * level);
-
-                case StatType.ArmorPenetration:
-                return baseStats.Get(StatType.ArmorPenetration) + (statPerLevel.Get(StatType.ArmorPenetration) * level);
-
-                case StatType.MagicPenetration:
-                return baseStats.Get(StatType.MagicPenetration) + (statPerLevel.Get(StatType.MagicPenetration) * level);
-
-                case StatType.SpellVamp:
-                return baseStats.Get(StatType.SpellVamp) + (statPerLevel.Get(StatType.SpellVamp) * level);
-
-                default:
-                return baseStats.Get(statType);
-            }
-        }
-
-        public void SetStat(StatType type, double value) {
+		public void SetBaseStat(StatType type, double value) {
             baseStats.Set(type, value);
         }
 
-        protected virtual void Test() {
-            throw new NotImplementedException();
-        }
+		public void SetStatPerLevel(StatType type, double value) {
+			statPerLevel.Set(type, value);
+		}
 
-        //--------------UtilityMethods-------------------
+        //--------------UtilityMethods------------------
+
+        /// <summary>
+        /// Levels abilities with prioritizing r, and then q->w->e. 
+        /// It levels the abilities in the same way as in LoL.
+        /// </summary>
+        /// <param name="level">Level of the champion</param>
         protected virtual void LevelAbilities(int level) {
-            throw new NotImplementedException();
+			//makes sure all abilites aren't leveled
+			qRank = 0;
+			wRank = 0;
+			eRank = 0;
+			rRank = 0;
+
+			for (int i = 0; i < level; i++) {
+				//level qwe to 1 first. Continue cuts the iteration short
+				if (qRank == 0) { qRank++; continue; }
+				if (wRank == 0) { wRank++; continue; }
+				if (eRank == 0) { eRank++; continue; }
+				//level r if level = 6, 11, 16
+				if (level == 6 || level == 11 || level == 16) { rRank++; continue; }
+
+				/* The rule for if abilities can be leveled up:
+                 * The current ability level cannot be higher than half the champion level(rounded up)
+                 * Aka: highest ability level = champion level/2 (rounded up)
+                 * For the sake of putting this into an if-statement the formular is modified
+                 * Instead of dividing champion level by 2, we instead multiply the ability level by 2
+                 * The only place this has effect is at level 8 where q=4 and level=8
+                 */
+				//prioritize leveling q
+				if (qRank < 5 && qRank * 2 < level) { qRank++; continue; }
+				//then prioritize leveling w
+				if (wRank < 5 && wRank * 2 < level) { wRank++; continue; }
+				//then prioritize leveling e
+				if (eRank < 5 && eRank * 2 < level) { eRank++; continue; }
+			}
+		}
+
+		/// <summary>
+		/// Sets the ability ranks to the parameters, but if the parameters aren't legal by being too big or small,
+		/// then the ranks are made legal. This is done by making ranks too big the maxRank,
+		/// and ranks too small the minRank.
+		/// </summary>
+		/// <param name="qRank">The rank/level of q</param>
+		/// <param name="wRank">The rank/level of w</param>
+		/// <param name="eRank">The rank/level of e</param>
+		/// <param name="rRank">The rank/level of r</param>
+		protected virtual void SetAbilityRanks(int qRank, int wRank, int eRank, int rRank) {
+            this.qRank = MakeRankLegal(qRank, 0, 5);
+            this.wRank = MakeRankLegal(wRank, 0, 5);
+            this.eRank = MakeRankLegal(eRank, 0, 5);
+            this.rRank = MakeRankLegal(rRank, 0, 3);
+            level = qRank + wRank + eRank + rRank;
         }
 
-    }
+        /// <summary>
+        /// Checks if the ability rank is within the legal rank range. 
+        /// If the rank is outside the range then the rank is changed to be the closest legal rank.
+        /// </summary>
+        /// <param name="rank">The rank to validate/make legal</param>
+        /// <param name="minRank">The minimum rank of the ability</param>
+        /// <param name="maxRank">The maximum rank of the ability</param>
+        /// <returns>Returns the closest legal rank</returns>
+        protected virtual int MakeRankLegal(int rank, int minRank, int maxRank) {
+			if (minRank <= rank && rank <= maxRank) { //Checks if the rank is legal
+                return rank;
+			} else if (rank > maxRank) { //Rank is too big, and returns the max rank
+				return maxRank;
+			} else { //Rank is too small, and returns the min rank
+				return minRank;
+			}
+		}
+
+        /// <summary>
+        /// Sets the baseStats and statsPerLevel statHandlers. Default sets everything to 0.
+        /// It should be overriden in every champion class, to specify the stats.
+        /// </summary>
+        protected virtual void SetBaseStats() {
+			baseStats = new StatHandler(new Dictionary<StatType, double>() {
+				{StatType.Health, 0},
+				{StatType.Mana, 0},
+				{StatType.AttackDamage, 0},
+				{StatType.Armor, 0},
+				{StatType.MagicResistance, 0},
+				{StatType.AttackSpeed, 0},
+				{StatType.MovementSpeed, 0},
+				{StatType.ManaRegeneration, 0},
+				{StatType.HealthRegeneration, 0}
+			});
+			statPerLevel = new StatHandler(new Dictionary<StatType, double>() {
+				{StatType.Health, 0},
+				{StatType.Mana, 0},
+				{StatType.AttackDamage, 0},
+				{StatType.Armor, 0},
+				{StatType.MagicResistance, 0},
+				{StatType.AttackSpeed, 0},
+				{StatType.MovementSpeed, 0},
+				{StatType.ManaRegeneration, 0},
+				{StatType.HealthRegeneration, 0}
+			});
+		}
+	}
 }
--- Champions/Champions/Specialist/Cho'Gath.cs
+++ Champions/Champions/Specialist/Cho'Gath.cs
@@ -15,32 +15,15 @@ namespace Champions.Champions.Specialist {
         public Cho_Gath(int level, int rStacks) {
             this.rStacks = rStacks;
             this.level = level;
-            this.LevelAbilities(level);
-
-            //------stats------
-            baseStats = new StatHandler(new Dictionary<StatType, double>() {
-                {StatType.Health, 644},
-                {StatType.Mana, 270},
-                {StatType.AttackDamage, 69},
-                {StatType.Armor, 38},
-                {StatType.MagicResistance, 32},
-                {StatType.AttackSpeed, 0.625},
-                {StatType.MovementSpeed, 345},
-                {StatType.ManaRegeneration, 7.2},
-                {StatType.HealthRegeneration, 9}
-            });
-            statPerLevel = new StatHandler(new Dictionary<StatType, double>() {
-                {StatType.Health, 94},
-                {StatType.Mana, 60},
-                {StatType.AttackDamage, 4.2},
-                {StatType.Armor, 5},
-                {StatType.MagicResistance, 2.05},
-                {StatType.AttackSpeed, 0},
-                {StatType.MovementSpeed, 345},
-                {StatType.ManaRegeneration, 0.45},
-                {StatType.HealthRegeneration, 0.85}
-            });
+            LevelAbilities(level);
+            SetBaseStats();
         }
+
+        public Cho_Gath(int qRank, int wRank, int eRank, int rRank, int rStacks) {
+            this.rStacks = rStacks;
+            SetAbilityRanks(qRank, wRank, eRank, rRank);
+			SetBaseStats();
+		}
 
         //--------------Abilities---------------
         public override Damage AutoAttack() {
@@ -61,36 +44,42 @@ namespace Champions.Champions.Specialist {
             throw new NotImplementedException();
         }
 
-        //------------UtilityMethods----------------
-        protected override void LevelAbilities(int level) {
-            //makes sure all abilites aren't leveled
-            qLevel = 0;
-            wLevel = 0;
-            eLevel = 0;
-            rLevel = 0;
-
-            for (int i = 0; i < level; i++) {
-                //level qwe to 1 first. Continue cuts the iteration short
-                if (qLevel == 0) { qLevel++; continue; } 
-                if (wLevel == 0) { wLevel++; continue; }
-                if (eLevel == 0) { eLevel++; continue; }
-                //level r if level = 6, 11, 16
-                if (level == 6 || level == 11 || level == 16) { rLevel++; continue; }
-
-                /* The rule for if abilities can be leveled up:
-                 * The current ability level cannot be higher than half the champion level(rounded up)
-                 * Aka: highest ability level = champion level/2 (rounded up)
-                 * For the sake of putting this into an if-statement the formular is modified
-                 * Instead of dividing champion level by 2, we instead multiply the ability level by 2
-                 * The only place this has effect is at level 8 where q=4 and level=8
-                 */
-                //prioritize leveling q
-                if (qLevel < 5 && qLevel * 2 < level) { qLevel++; continue; }
-				//then prioritize leveling w
-				if (wLevel < 5 && wLevel * 2 < level) { wLevel++; continue; }
-				//then prioritize leveling e
-				if (eLevel < 5 && eLevel * 2 < level) { eLevel++; continue; }
+		//------------Getters&Setters---------------
+		public override double GetStat(StatType statType) {
+			switch (statType) {
+				case StatType.Health:
+					//rStacks calculation looks weird but (40 + 40 * rRank) is to follow the bonus hp pr rRank which is 80/120/160
+					return baseStats.Get(statType) + (statPerLevel.Get(statType) * level) + (rStacks * (40 + 40 * rRank));
+				default:
+					return base.GetStat(statType);
 			}
-        }
-    }
+		}
+
+		//------------UtilityMethods----------------
+
+		protected override void SetBaseStats() {
+			baseStats = new StatHandler(new Dictionary<StatType, double>() {
+				{StatType.Health, 644},
+				{StatType.Mana, 270},
+				{StatType.AttackDamage, 69},
+				{StatType.Armor, 38},
+				{StatType.MagicResistance, 32},
+				{StatType.AttackSpeed, 0.625},
+				{StatType.MovementSpeed, 345},
+				{StatType.ManaRegeneration, 7.2},
+				{StatType.HealthRegeneration, 9}
+			});
+			statPerLevel = new StatHandler(new Dictionary<StatType, double>() {
+				{StatType.Health, 94},
+				{StatType.Mana, 60},
+				{StatType.AttackDamage, 4.2},
+				{StatType.Armor, 5},
+				{StatType.MagicResistance, 2.05},
+				{StatType.AttackSpeed, 0},
+				{StatType.MovementSpeed, 345},
+				{StatType.ManaRegeneration, 0.45},
+				{StatType.HealthRegeneration, 0.85}
+			});
+		}
+	}
 }
